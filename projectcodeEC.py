57cb2f94a3b820330273240c73d36da533174f79
Emmanuel J Cox <ecox143@unm.edu>
2024-12-06 21:20:32 -0700

    Update projectcodeEC.py

--- projectcodeEC.py
+++ projectcodeEC.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 st.title("CSV Data Visualization App")
 
 # File uploader for CSV
-uploaded_file = st.file_uploader("NM_24_NO2.csv", type=["csv"])
+uploaded_file = st.file_uploader('NM_24_NO2.csv', type=["csv"])
 
 if uploaded_file is not None:
     # Read the CSV file
